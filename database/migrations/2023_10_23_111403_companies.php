45ea06a425085aae793c011ea2c076723c937987
salman143320 <44635760+salman143320@users.noreply.github.com>
2023-10-25 09:14:57 +0530

    Update 2023_10_23_111403_companies.php

--- database/migrations/2023_10_23_111403_companies.php
+++ database/migrations/2023_10_23_111403_companies.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('logo');
+            $table->string('website');
             $table->timestamps();
         });
     }
